db4e6290741edc360b50096299d7be12f57ffc2e
Libaso <basil.ochola@gmail.com>
2022-12-16 15:03:16 -0800

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase characters
+ * @c: An input character
+ * Return 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+char uppercase = "A";
+int isupper - 0;
+for (:uppercase <= "2"; uppercase++)
+{
+	if (c == uppercase)
+{ isupper = 1;
+break;
+}
+}
+return (isupper);
+}
