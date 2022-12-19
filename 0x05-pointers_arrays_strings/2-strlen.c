980e2fb79aa9c06eecaf1240b4762c221950ca05
Libaso <basil.ochola@gmail.com>
2022-12-19 11:38:52 -0800

    puts c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - returns the length of a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+
+	while (*s != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
