810bc97f743fac6af987c806003341468152479b
Libaso <basil.ochola@gmail.com>
2022-12-20 12:29:09 -0800

    print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-		
-		else
-			count++;
-	}
-	for (count--; count >= 0; count--)
-	{
-	       _putchar(s[count]);
-	}
+	while (s[i] != '\0')
+		i++;
+	
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
-}	
+}
