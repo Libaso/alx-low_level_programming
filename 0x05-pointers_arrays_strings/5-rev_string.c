3e4c702b63cb99278d6261b7fd469ef250455f1e
Libaso <basil.ochola@gmail.com>
2022-12-20 14:36:31 -0800

    rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,16 @@
  */
 void rev_string(char *s)
 {
-	char rv = s[0];
+	char rev = s[0];
 	int c = 0;
 	int i;
 
 	while (s[c] != '\0')
 		c++;
-	for(i = 0; i < c; i++)
+	for (i = 0; i < c; i++)
 	{
 		c--;
-		rv = s[i];
+		rev = s[i];
 		s[i] = s[c];
 		s[c] = r[v];
 }
